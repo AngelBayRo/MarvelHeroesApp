ff41cc51f763dcdd9743c2420b87997ac9a55eec
Abayonro <angel.bayon@accenture.com>
2021-09-27 16:26:21 +0200

    updating

---
+++ MarvelHeroesApp/MarvelHeroesApp/Modules/Home/Models/SuperheroServerModel.swift
@@ -0,0 +1,86 @@
+//
+//  SuperheroModel.swift
+//  MarvelHeroesApp
+//
+//  Created by angel.bayon on 26/9/21.
+//
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let superheroes = try? newJSONDecoder().decode(Superheroes.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Superheroes
+struct Superheroes: Codable {
+    let code: Int
+    let status, copyright, attributionText: String
+    let attributionHTML: String
+    let data: DataClass
+    let etag: String
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let offset, limit, total, count: Int
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let id: Int
+    let name, resultDescription, modified: String
+    let resourceURI: String
+    let urls: [URLElement]
+    let thumbnail: Thumbnail
+    let comics: Comics
+    let stories: Stories
+    let events, series: Comics
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case resultDescription = "description"
+        case modified, resourceURI, urls, thumbnail, comics, stories, events, series
+    }
+}
+
+// MARK: - Comics
+struct Comics: Codable {
+    let available, returned: Int
+    let collectionURI: String
+    let items: [ComicsItem]
+}
+
+// MARK: - ComicsItem
+struct ComicsItem: Codable {
+    let resourceURI, name: String
+}
+
+// MARK: - Stories
+struct Stories: Codable {
+    let available, returned: Int
+    let collectionURI: String
+    let items: [StoriesItem]
+}
+
+// MARK: - StoriesItem
+struct StoriesItem: Codable {
+    let resourceURI, name, type: String
+}
+
+// MARK: - Thumbnail
+class Thumbnail: Codable {
+    let path, thumbnailExtension: String
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
+
+// MARK: - URLElement
+struct URLElement: Codable {
+    let type, url: String
+}
+
+
